36d24e76bb8314b0c918f4c76740a896cb6e5430
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2022-09-04 09:16:50 +0200

    Initial Commit

---
+++ Image-SwiftUI/Image_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_SwiftUIApp.swift
+//  Image-SwiftUI
+//
+//  Created by Macbook on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Image_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

e1fd7e610a735f22d42a9c9a8495cf4276107d6c
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-06 14:09:15 +0200

    Second Excercise on Applying overlay on Image-View

--- Image-SwiftUI/ContentView.swift
+++ Image-SwiftUI/ContentView.swift
@@ -12,18 +12,17 @@ struct ContentView: View {
         Image("Eiffel tower")
             .resizable() // To Resize the Image.
             .aspectRatio(contentMode: .fit)
+            .frame(width: 350)
             //Applying Overlay on an Image..
             .overlay(
-                Text("If you are lucky enough to have lived in paris as a young man, then wherever you go far the rest of your life it stays with you, for paris is a moveable feast .\n \n -Ernest hemingway")
-                    .fontWeight(.heavy)
-                    .font(.system(.headline, design: .rounded))
+            Rectangle()
+                .opacity(0.3)
+                .overlay(
+                Text("Paris")
+                    .font(.system(size: 50, weight: .black, design: .rounded))
                     .foregroundColor(.white)
-                    .padding()
-                    .background(Color.black)
-                    .cornerRadius(10)
-                    .opacity(0.8)
-                    .padding(),
-                    alignment: .top
+                    .frame(width: 200)
+                )
             )
             
     }
